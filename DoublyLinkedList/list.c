605296cbe3eb37dd17c0beeb98e07a72b1d40373
LordCalm <LordCalm@users.noreply.github.com>
2018-11-23 01:29:15 +0300

    Добавлена безопасность функций в плане проверки указателей, индексов и т.д.

--- DoublyLinkedList/list.c
+++ DoublyLinkedList/list.c
@@ -12,13 +12,18 @@ typedef struct list {
 	link tail;
 	int size;
 }list;
+
+void MemoryError();
+
+list* CreateList();
 list* ArrayToList(int *Array, int size);
 
 void printInt(int value);
 void PrintList(list *list, void(*fun)(void*));
 
-void InsertAfter(list *List, int index, int data);
-void insertBefore(list *List, int index, int data);
+void InsertFiled(int data);
+void InsertAfter(list **List, int index, int data);
+void InsertBefore(list **List, int index, int data);
 
 void Delete(list **List, int index);
 void DeleteList(list **List);
@@ -33,20 +38,31 @@ int main()
 {
 	int M[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	int size_M = sizeof(M) / sizeof(*M);
-	list *L = ArrayToList(M, size_M);
-	InsertAfter(L, 0, 20);
-	insertBefore(L, 3, 100);
-	Delete(&L, 1);
+	list *L = CreateList();
+	//list *L = ArrayToList(M, size_M);
+	InsertAfter(&L, 0, 1);
+	InsertAfter(&L, 1, 20);
+	InsertAfter(&L, 2, 20);
+	InsertBefore(&L, 3, 100);
+	//Delete(&L, 1);
 	//DeleteList(&L);
 	//printf("%i\n", FindIndex(L, 100));
-	BubbleSort(&L);
+	//BubbleSort(&L);
 	PrintList(L, printInt);
 	system("pause");
 	return 0;
 }
+
+void MemoryError()
+{
+	printf("No memory\n");
+	exit(1);
+}
+
 list* CreateList()
 {
 	list *tmp = (list*)malloc(sizeof(list));
+	if (tmp == NULL) MemoryError();
 	tmp->size = 0;
 	tmp->head = tmp->tail = NULL;
 	return tmp;
@@ -54,7 +70,9 @@ list* CreateList()
 list* ArrayToList(int *Array, int size)
 {
 	item *cur = (item*)calloc(1, sizeof(item));
+	if (cur == NULL) MemoryError();
 	list *List = (list*)calloc(1, sizeof(list));
+	if (List == NULL) MemoryError();
 	List->head = cur;
 	List->size = size;
 	for (int i = 0; i < size; i++)
@@ -63,6 +81,7 @@ list* ArrayToList(int *Array, int size)
 		if (i < size - 1)
 		{
 			cur->next = (item*)calloc(1, sizeof(item));
+			if (cur->next == NULL) MemoryError();
 			cur->next->prev = cur;
 		}
 		else cur->next = NULL;
@@ -87,35 +106,91 @@ void PrintList(list *list, void (*fun)(void*))
 	printf("\n");
 }
 
-void InsertAfter(list *List, int index, int data)
+void InsertFiled(int data)
 {
-	(List->size)++;
-	link cur = List->head;
-	for (int i = 1; i < index; i++)
-	{
-		cur = cur->next;
-	}
-	link el = (item*)calloc(1, sizeof(item));
-	el->data = data;
-	el->next = cur->next;
-	el->prev = cur;
-	cur->next = el;
-	(el->next)->prev = el;
+	printf("%i is not inserted.\n", data);
+	system("pause");
+	exit(1);
 }
-void insertBefore(list *List, int index, int data)
+void InsertAfter(list **List, int index, int data)
 {
-	(List->size)++;
-	link cur = List->head;
-	for (int i = 1; i < index; i++)
+	if (data)
 	{
-		cur = cur->next;
+		if ((*List)->head != NULL)
+		{
+			if (0 < index && index <= (*List)->size)
+			{
+				((*List)->size)++;
+				link cur = (*List)->head;
+				for (int i = 1; i < index; i++)
+				{
+					cur = cur->next;
+				}
+				link el = (item*)calloc(1, sizeof(item));
+				if (el == NULL) MemoryError();
+				el->data = data;
+				el->next = cur->next;
+				el->prev = cur;
+				cur->next = el;
+				if (el->next != NULL) (el->next)->prev = el;
+			}
+			else InsertFiled(data);
+		}
+		else
+		{
+			if (0 <= index)
+			{
+				((*List)->size)++;
+				link el = (item*)calloc(1, sizeof(item));
+				if (el == NULL) MemoryError();
+				el->data = data;
+				(*List)->head = el;
+				(*List)->tail = el;
+			}
+			else InsertFiled(data);
+		}
 	}
-	link el = (item*)calloc(1, sizeof(item));
-	el->data = data;
-	el->next = cur;
-	el->prev = cur->prev;
-	cur->prev->next = el;
-	cur->prev = el;
+	else InsertFiled(data);
+
+}
+void InsertBefore(list **List, int index, int data)
+{
+	if (data)
+	{
+		if ((*List)->head != NULL)
+		{
+			if (index == 1)
+			{
+				((*List)->size)++;
+				link cur = (*List)->head;
+				link el = (item*)calloc(1, sizeof(item));
+				if (el == NULL) MemoryError();
+				el->data = data;
+				el->next = cur;
+				cur->prev = el;
+				(*List)->head = el;
+			}
+			else if (1 < index && index <= (*List)->size)
+			{
+				((*List)->size)++;
+				link cur = (*List)->head;
+				for (int i = 1; i < index; i++)
+				{
+					cur = cur->next;
+				}
+				link el = (item*)calloc(1, sizeof(item));
+				if (el == NULL) MemoryError();
+				el->data = data;
+				el->next = cur;
+				el->prev = cur->prev;
+				cur->prev->next = el;
+				cur->prev = el;
+			}
+			else InsertFiled(data);
+		}
+		else InsertFiled(data);
+	}
+	else InsertFiled(data);
 }
 
 void Delete(list **List, int index)
@@ -174,22 +249,31 @@ void Swap(link **ptr1, link **ptr2)
 }
 void BubbleSort(list **list)
 {
-	link cur = (*list)->head;
-	int check = 1;
-	while (check != 0)
+	if (list != NULL)
 	{
-		int check1 = 0;
-		while(cur->next != NULL)
+		link cur = (*list)->head;
+		int check = 1;
+		while (check != 0)
 		{
-			if (cur->data > (cur->next)->data)
+			int check1 = 0;
+			while (cur->next != NULL)
 			{
-				Swap(cur, cur->next);
-				check1++;
+				if (cur->data > (cur->next)->data)
+				{
+					Swap(cur, cur->next);
+					check1++;
+				}
+				cur = cur->next;
 			}
-			cur = cur->next;
+			if (check1 == 0) check = 0;
+			cur = (*list)->head;
 		}
-		if (check1 == 0) check = 0;
-		cur = (*list)->head;
+		(*list)->head = cur;
 	}
-	(*list)->head = cur;
+	else
+	{
+		printf("BubbleSort is not performed.\n");
+		system("pause");
+		exit(1);
+	}
 }
