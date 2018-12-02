f8f95a8faa75fcd1239ea5cfbaa2649fca1dde73
LordCalm <LordCalm@users.noreply.github.com>
2018-12-02 18:20:53 +0300

    Последняя версия на С.

--- DoublyLinkedList/DoublyLinkedList.h
+++ DoublyLinkedList/DoublyLinkedList.h
@@ -19,34 +19,6 @@ typedef struct list {
 	link tail;
 	int size;
 }list;
-/*
-void ListOK(list *L);
-size_t Checksum(link el);
-void OK(link el);
-
-void MemoryError();
-link CreateNode();
-
-list* CreateList();
-
-void printFloat(link cur);
-void printInt(link cur);
-void PrintList(list *list, void(*fun)(link));
-
-void InsertFiled(type data);
-void InsertAfter(list **List, int index, type data);
-void InsertBefore(list **List, int index, type data);
-list* ArrayToList(type *Array, int size);
-
-void Delete(list **List, int index);
-void DeleteList(list **List);
-
-link FindPtr(list *list, type data);
-int FindIndex(list *list, type data);
-
-void Swap(link *ptr1, link *ptr2);
-void BubbleSort(list **list);
-*/
 void MemoryError()
 {
 	printf("No memory\n");
@@ -65,7 +37,6 @@ void ListOK(list *L)
 		if (L->head != NULL && L->tail != NULL)
 			return;
 	printf("List not found.\n");
-	//system("pause");
 	exit(1);
 }
 size_t Checksum(link el)
@@ -97,7 +68,6 @@ void OK(link el)
 		if (el->can1 != CAN || el->can2 != CAN || Checksum(el) != el->checksum)
 		{
 			printf("Error\n");
-			//system("pause");
 			exit(1);
 		}
 	}
@@ -135,7 +105,6 @@ void PrintList(list *list, void(*fun)(link))
 void InsertFiled(type data)
 {
 	printf("%i is not inserted.\n", data);
-	//system("pause");
 	exit(1);
 }
 void InsertAfter(list **List, int index, type data)
@@ -243,7 +212,6 @@ list* ArrayToList(type *Array, int size)
 	else
 	{
 		printf("Array is empty.\n");
-		//system("pause");
 		exit(1);
 	}
 }
@@ -285,7 +253,6 @@ void Delete(list **List, int index)
 		return;
 	}
 	printf("%i is not deleted.\n", index);
-	//system("pause");
 	exit(1);
 }
 void DeleteList(list **List)
@@ -357,7 +324,6 @@ void BubbleSort(list **list)
 	else
 	{
 		printf("BubbleSort is not performed.\n");
-		//system("pause");
 		exit(1);
 	}
 }
